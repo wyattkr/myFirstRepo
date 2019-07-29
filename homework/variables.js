e34e949189bcbdc7dd7edead870b63e0f8178ef1
wyattkr <wkim37889@gmail.com>
2019-07-29 11:29:00 -0500

    new Webpage

---
+++ homework/variables.js
@@ -0,0 +1,40 @@
+var firstString = "";
+var secondString = "Im not that old im only 2";
+var middleName = "roshel";
+var parrotName = "someName";
+var dogName = "effile";
+var paragraph = "this is a hard to read paragraph";
+// Above are the examples of a String.
+//Strings can represent words, paragraphs, or
+// anything that needs to be text.
+
+var num1, num2, num3;
+// Above shows how to create multiple variables at once.
+// very useful when trying yo keep lines used in code
+// at minimum.  Below shows how to use those variables....
+num1 = 1000;
+num2 = 100000;
+num3 = 100000000;
+
+var name, carCapacity, region;
+name = 100;
+carCapacity = "six people";
+region = name + carCapacity;
+// Variables dont have to be created for the name
+// Create variables to hold data.
+var smallDecimal = 10.46;
+var anotherDecimal = 1.3;
+var decimalThreee = 33.02;
+var twoD = anotherDecimal * decimalThreee;
+var threeDEE = twoD + anotherDecimal;
+// Above represents the data type Decimal in
+// use with variables.
+var notTrue = true;
+var soTrue = false;
+var theznextOne = notTrue;
+var anotherBoolean = true;
+// Boolean are True  False values.  They are very useful
+// inside of conditions.  In order for you to be able to 
+// enhance your code and make it a little more responsive 
+// you need to add conditionals. True / False conditions
+//  that start or stop your code. 
