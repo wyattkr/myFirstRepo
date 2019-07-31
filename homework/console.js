3e354e3fc456ab38a0c240a8f7f8b8dee361a112
wyattkr <wkim37889@gmail.com>
2019-07-31 14:27:20 -0500

    object homework

---
+++ homework/console.js
@@ -0,0 +1,3 @@
+var dateofBirth = newDate("May 24, 2016 9:00:00");
+console.log(dateofBirth());
+
--- homework/noHTML.js
+++ homework/noHTML.js
@@ -1,4 +1,20 @@
 var addingNumbers = 234 + 45;
 console.log(addingNumbers);
 
+console.log("Hello World");
+var date =newDate("August 1,2019");
+document.write(...text: toString);
+console.log(newdate);
+
+var d = "July 31";
+console.log (typeof date);  =String
+
+var convertDate = July 31,2019
+console.log (convertDate);
+console.log(string(convertDate));
+
+
+
+
+
 
