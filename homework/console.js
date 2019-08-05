695bd704d2773a6d9bd20afc02e75ecba1a0bdaa
wyattkr <wkim37889@gmail.com>
2019-08-05 15:55:19 -0500

    practice work

--- homework/console.js
+++ homework/console.js
@@ -1,3 +1,15 @@
-var dateofBirth = newDate("May 24, 2016 9:00:00");
-console.log(dateofBirth());
 
+
+var Five = 100 ;
+Five.toExponential
+var a = Five.toLocaleString();
+console.log(a)
+
+var Five = 100
+var a = Five.toExponential();
+console.log(a)
+
+var twenty = 20.75
+twenty.toFixed
+var x = twenty.toFixed();
+console.log(x)
