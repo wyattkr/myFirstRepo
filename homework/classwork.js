869a785b8246e62ed8740d7a53bcd9f6c106bbbd
wyattkr <wkim37889@gmail.com>
2019-08-07 18:19:20 -0500

    loop homework

---
+++ homework/classwork.js
@@ -0,0 +1,5 @@
+for(var i=0;i<25;i++){
+    var text="";
+    text +="Kim One";
+    console.log(text);
+}
---
+++ homework/loopedHomework.js
@@ -0,0 +1,10 @@
+var text ="";
+var i = 0;
+do {
+    text += "the number is" +i;
+    i++;
+}
+while (i>10)
+console.log(text);
+
+    
