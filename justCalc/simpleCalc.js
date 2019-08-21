50c45ae6f1e8874bc597738cda98f45ef6a50161
wyattkr <wkim37889@gmail.com>
2019-08-21 17:55:03 -0500

    Calculator homework

---
+++ justCalc/simpleCalc.js
@@ -0,0 +1,35 @@
+var x;
+var y;
+var z;
+
+function putTheNumbsTogether(x,y) {
+    z = x +y;
+    console.log(z)
+}
+putTheNumbsTogether(3, 7);
+
+function seperateTheNumbsTogether(x,y){
+    z = x-y;
+    console.log(z)
+}
+seperateTheNumbsTogether(7,3)
+
+function multiplyTheNumbsTogether(x,y) {
+    z = x*y;
+    console.log(z)
+}
+multiplyTheNumbsTogether(7,3);
+
+function quotientofTheNumbsTogether(y,x) {
+    z = x/y;
+    console.log(z)
+}
+quotientofTheNumbsTogether(3,7);
+
+function incrementofTheNumbsTogether(x,y){
+    z = x,y ;
+    z = ++z
+    console.log(++z)
+}
+incrementofTheNumbsTogether(7,3);
+
