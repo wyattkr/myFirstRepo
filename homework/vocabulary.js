6580c82b94c3a8a204eef18cdbd0656a77908de2
wyattkr <wkim37889@gmail.com>
2019-08-26 21:01:27 -0500

    practice work and class work

---
+++ homework/vocabulary.js
@@ -0,0 +1,63 @@
+// Function- allows you to make JavaScript code more efficent 
+// and more reusable.
+
+// Onload- means the browser has finished loading the page
+
+// .innerHTML- property is used to change the text of the 
+//paragraph element accordingly.
+
+// script- tag is used to define JavaScript from
+// within an HTML file.
+
+// HTML- declares the structure and content of your page.
+
+// Body-element contains all the content of an HTML document.
+
+// Event- are aspects which occur in a program.  Code can
+// be excuted whenever an event is triggered in the HTML program.
+
+// Alert- this function will display text in a dialog box that
+// pop up on the screen.
+
+// Handlers- are JavaScript applications in the navigator.
+
+// Onclick - is an HTML event which allows the user to click
+// on the event.
+
+// Data- is factual information, measurements or statistics
+// used as a basis for reasoning, discussion, or calculation.
+
+// Console - is used to print any kind of variables.
+
+// Boolean- is a type of expression used to represent the Boolean 
+// value of true or false.
+
+// Number - These are variables that are used to hold numbers which can be 
+// changed at any point in time in the program.
+
+// += - This carries out the addition and assignment operator
+// in one go.
+
+// /= - This carries out the division and assignment operator
+// in one go.
+
+// == - This is used to check if two operands are equal.
+
+// Value - are numbers, some values are pieces of text, some
+// values are functions.
+
+// getElementById - method returns the element that has the Id 
+// attribute with the specified value.
+
+// If - this statement allows you to make decisions in your code
+// to excute the code.
+
+// else - this statement also allow you to make decisions in your
+// code to excute the code.
+
+// Constant - a constant can be global or local to a function
+// where it is declared- constants are read only and can't
+// be modified later.
+
+//  Var-  is the keyword to declare a new variable and the
+// assignment operator,=, to assign a value to it.
